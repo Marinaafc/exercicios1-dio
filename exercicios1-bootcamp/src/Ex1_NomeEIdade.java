051c8163750c2b21d0017cd52936128c1d6ea3a4
Marinaafc <marinaafc.99@gmail.com>
2023-03-18 11:48:54 -0300

    Adicionando descrição do exercício e alterando nome do arquivo

---
+++ exercicios1-bootcamp/src/Ex1_NomeEIdade.java
@@ -0,0 +1,12 @@
+public class Ex1_NomeEIdade {
+    public static void main(String[] args) throws Exception {
+
+/*
+ * Faça um programa que leia conjuntos de dois valores,
+ * o primeiro representando o nome do aluno e o segundo representando a sua idade.
+ * (Pare inserindo o valor 0 no campo nome)
+ */
+
+
+    }
+}
