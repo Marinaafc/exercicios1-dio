5a5cead7788a13735c96f4dad15d65a1178a49d9
Marinaafc <marinaafc.99@gmail.com>
2023-03-19 21:07:46 -0300

    Finalizando exercício 6

---
+++ exercicios1-bootcamp/src/Ex6_Fatorial.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Ex6_Fatorial {
+    
+/*
+ * Faça um programa que calcule o fatorial de um 
+ * número inteiro fornecido pelo usuário.
+ * Ex.: 5! = 120 (5x4x3x2x1)
+ */
+
+public static void main(String[] args) {
+
+    Scanner scan = new Scanner(System.in);
+
+    System.out.println("Fatorial: ");
+    long fatorial = scan.nextLong();
+
+    long multiplicacao = 1L;
+
+    for(long i = fatorial; i >= 1 ; i--){
+        multiplicacao *= i;
+
+    }
+    System.out.println(fatorial +"! = " + multiplicacao);
+}
+
+}
