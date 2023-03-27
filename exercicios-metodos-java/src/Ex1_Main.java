96ddf3c783796fec355614fdabea5e13601f1973
Marinaafc <marinaafc.99@gmail.com>
2023-03-26 21:02:23 -0300

    Adicionando Ex1

---
+++ exercicios-metodos-java/src/Ex1_Main.java
@@ -0,0 +1,17 @@
+
+public class Ex1_Main {
+    
+    public static void main(String[] args) {
+        
+
+        // Calculadora
+        System.out.println("Exercício calculadora");
+        Ex1_Calculadora.soma(3,6);
+        Ex1_Calculadora.subtracao(9,1.8);
+        Ex1_Calculadora.multiplicacao(7,8);
+        Ex1_Calculadora.divisao(5,2.5);
+
+        // Mensagem
+        System.out.println("Exercício mensagem");
+    }
+}
